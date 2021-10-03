acb4c3f78e56519a4898ba2af0e24126d7d2d26d
Ilya Baskakov <baskakov.11111@mail.ru>
2021-10-03 21:20:29 +0300

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import mongoose from "mongoose"
+import fileUpload from 'express-fileupload'
+import router from './router.js'
+
+const PORT = 5000
+const DB_URL = 'mongodb+srv://user:user@cluster0.mly3y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
+app.use('/api', router)
+
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+        app.listen(PORT, () => {
+            console.log('SERVER STARTED ON PORT 5000')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+startApp()
